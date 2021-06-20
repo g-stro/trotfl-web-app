25849e7b66ad15b113d4e6fb22370124818ac074
g-stro <gstroud@live.co.uk>
2021-06-20 22:42:22 +0100

    Initial commit from Spring Initializr

---
+++ src/main/java/com/trotfl/trotflwebapp/TrotflWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.trotfl.trotflwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrotflWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrotflWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trotfl/trotflwebapp/TrotflWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trotfl.trotflwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrotflWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
